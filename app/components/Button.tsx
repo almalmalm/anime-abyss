db2a660fd5ebf3edea80c9e4009f14b5e9490d1c
almalmalm <almostkalinin@gmail.com>
2023-07-28 23:38:45 +0400

    init: create homepage template

---
+++ app/components/Button.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Button = () => {
+  return <button className="border border-rose-700">Search</button>;
+};
---
+++ app/components/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Input } from './Input';
+import { Button } from './Button';
+
+export const Search = () => {
+  return (
+    <div className="flex justify-center">
+      <form action="" className="flex gap-2">
+        <Input />
+        <Button />
+      </form>
+    </div>
+  );
+};
